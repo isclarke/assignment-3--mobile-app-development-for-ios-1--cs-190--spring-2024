8d72cbe0e09ae609ba876e7f3d0e0b366766c455
isclarke <156957402+isclarke@users.noreply.github.com>
2024-04-30 16:27:25 -0400

    add rest of code

--- GameCode.swift
+++ GameCode.swift
@@ -1,17 +1,28 @@
 import Foundation
 
 let ball = OvalShape(width: 40, height: 40)
-let barrierWidth = 300.0
-let barrierHeight = 25.0
 
-let barrierPoints = [
-    Point(x: 0, y: 0),
-    Point(x : 0, y: barrierHeight),
-    Point(x: barrierWidth, y: barrierHeight),
-    Point(x: barrierWidth, y: 0 )
-]
-let barrier = PolygonShape(points: barrierPoints)
+var barriers: [Shape] = []
+var targets: [Shape] = []
 
+fileprivate func addBarrier(at position: Point, width: Double, height: Double, angle: Double ) {
+    let barrierPoints = [
+        Point(x: 0, y: 0),
+        Point(x: 0, y: height),
+        Point(x: width, y: height),
+        Point(x: width, y: 0)
+    ]
+    let barrier = PolygonShape(points: barrierPoints)
+    
+    barriers.append(barrier)
+    
+    barrier.position = position
+    barrier.hasPhysics = true
+    scene.add(barrier)
+    barrier.isImmobile = true
+    barrier.fillColor = .brown
+    barrier.angle = angle
+}
 let funnelPoints = [
         Point(x: 0, y: 50),
         Point(x: 80, y: 50),
@@ -20,24 +31,28 @@ let funnelPoints = [
 ]
 let funnel = PolygonShape(points: funnelPoints)
 
-let targetPoints = [
-    Point(x: 10, y: 0),
-    Point(x: 0, y: 10),
-    Point(x: 10, y: 20),
-    Point(x: 20, y: 10)
-]
-let target = PolygonShape(points: targetPoints)
-
-func setupTarget() {
-        target.position = Point(x: 200, y: 400)
-        target.hasPhysics = true
-        target.isImmobile = true
-        target.isImpermeable = false
-        target.fillColor = .yellow
+    func addTarget(at position: Point) {
+    let targetPoints = [
+        Point(x: 10, y: 0),
+        Point(x: 0, y: 10),
+        Point(x: 10, y: 20),
+        Point(x: 20, y: 10)
+    ]
+        
+    let target = PolygonShape(points:
+       targetPoints)
+        
+    targets.append(target)
     
-        scene.add(target)
-        target.name = "target"
-        target.isDraggable = false
+    target.position = position
+    target.hasPhysics = true
+    target.isImmobile = true
+    target.isImpermeable = false
+    target.fillColor = .yellow
+    
+    scene.add(target)
+    target.name = "target"
+    target.isDraggable = false
 }
 
 func ballCollided(with otherShape: Shape) {
@@ -49,6 +64,10 @@ func ballCollided(with otherShape: Shape) {
 func setupBall() {
     ball.onCollision = ballCollided(with:)
     ball.isDraggable = false
+    scene.trackShape(ball)
+    ball.onExitedScene = ballExitedScene
+    ball.onTapped = resetGame
+    ball.bounciness = 0.6
 }
 
 func setupFunnel() {
@@ -56,30 +75,67 @@ func setupFunnel() {
     scene.add(funnel)
     funnel.onTapped = dropBall
     funnel.fillColor = .gray
+    funnel.isDraggable = false
 }
-
-func setup() {
-    ball.position = Point(x: 250, y: 400)
-    scene.add(ball)
-    ball.hasPhysics = true
-    ball.fillColor = .blue
     
-    barrier.position = Point(x: 200, y: 150)
-    barrier.hasPhysics = true
-    scene.add(barrier)
-    barrier.isImmobile = true
-    barrier.fillColor = .brown
-    
-
-    setupFunnel()
-    setupTarget()
-    setupBall()
-   
-    
+    func dropBall() {
+        ball.position = funnel.position
+        ball.stopAllMotion()
+        
+        for barrier in barriers {
+            barrier.isDraggable = false
+        }
+        for target in targets {
+            target.fillColor = .yellow
+        }
     }
-
-func dropBall() {
-    ball.position = funnel.position
-    ball.stopAllMotion()
-}
+    
+    func ballExitedScene() {
+        for barrier in barriers {
+            barrier.isDraggable = true
+        }
+        var hitTargets = 0
+        for target in targets {
+            if target.fillColor == .green {
+                hitTargets += 1
+            }
+            if hitTargets == targets.count {
+                scene.presentAlert(text: "You won dude!", completion: alertDismissed)
+                
+                func alertDismissed(){
+                }
+            }
+                            
+        }
+    }
+    func resetGame() {
+        ball.position = Point(x: 0, y: -80)
+    }
+    
+    func printPosition(of shape: Shape) {
+        print(shape.position)
+    }
+    
+    func setup() {
+        ball.position = Point(x: 250, y: 400)
+        scene.add(ball)
+        ball.hasPhysics = true
+        ball.fillColor = .blue
+    
+        addBarrier(at: Point(x: 175, y: 100), width: 80, height: 25, angle: 0.1)
+        addBarrier(at: Point(x: 100, y: 150), width: 30,height: 15, angle: -0.2)
+        addBarrier(at: Point(x: 325, y: 150), width: 100,height: 25, angle: 0.03)
+        
+        addTarget(at: Point(x: 184, y: 563))
+        addTarget(at: Point(x: 238, y: 624))
+        addTarget(at: Point(x: 269, y: 453))
+        addTarget(at: Point(x: 213, y: 348))
+        addTarget(at: Point(x: 113, y: 267))
+      
+        setupFunnel()
+        setupBall()
+        resetGame()
+        
+        scene.onShapeMoved = printPosition(of:)
+    }
 
