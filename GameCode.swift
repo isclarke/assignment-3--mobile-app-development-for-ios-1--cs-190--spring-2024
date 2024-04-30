65656338824bac8b6c7aaa9f6fb5abf5c69a8a45
isclarke <156957402+isclarke@users.noreply.github.com>
2024-04-30 15:08:48 -0400

    add more code from book

--- GameCode.swift
+++ GameCode.swift
@@ -1,7 +1,31 @@
 import Foundation
 let circle = OvalShape(width: 150, height: 150)
+let barrierWidth = 300.0
+let barrierHeight = 25.0
 
+let barrierPoints = [
+    Point(x: 0, y: 0),
+    Point(x : 0, y: barrierHeight),
+    Point(x: barrierWidth, y: barrierHeight),
+    Point(x: barrierWidth, y: 0 )
+]
+let barrier = PolygonShape(points: barrierPoints)
+
+let funnelPoints = [
+        Point(x: 0, y: 50),
+        Point(x: 80, y: 50),
+        Point(x: 60, y: 0),
+        Point(x: 20, y: 0)
+]
+let funnel = PolygonShape(points: funnelPoints)
 func setup() {
     circle.position = Point(x: 250, y: 400)
     circle.hasPhysics = true
+    
+    barrier.position = Point(x: 200, y: 150)
+    barrier.isImmobile = true
+    scene.add(barrier)
+    
+    funnel.position = Point(x: 200, y: scene.height - 25)
+    scene.add(funnel)
 }
